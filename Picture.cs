85ff950b9a16446da2a2290219fe8618260258bf
pauluxxx <pauluxxx@PAVEL-PC>
2017-04-20 14:02:46 +0300

    s

---
+++ Picture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject.DAL.Entities.Interfaces;
+
+namespace TeamProject.DAL.Entities
+{
+    // Will be keeping in file system.
+    public class Picture : IEntity
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int MovieID { get; set; }
+
+        [ForeignKey("MovieID")]
+        public Movie Movie { get; set; }
+    }
+}
---
+++ PictureRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject.DAL.Entities;
+using TeamProject.DAL.Repositories.Interfaces;
+
+namespace TeamProject.DAL.Repositories
+{
+    public class PictureRepository : IRepository<Picture>
+    {
+        private CinemaContext db;
+
+        public IQueryable<Picture> Items 
+            =>  db.Pictures; 
+
+        // Maybe we should create new CinemaContext();
+        public PictureRepository(CinemaContext db)
+        {
+            this.db = db;
+        }
+
+        public Picture Create(Picture picture)
+            => db.Pictures.Add(picture);           
+        
+
+        public void Delete(Picture picture)
+            => db.Pictures.Remove(picture);
+
+        public IEnumerable<Picture> GetAll()
+            => db.Pictures.ToList();
+
+        public Picture Get(int id)
+            => db.Pictures.SingleOrDefault(view => view.ID == id);
+
+        public void Update(Picture picture)
+            => db.Set<Picture>().AddOrUpdate(picture);
+    }
+}
